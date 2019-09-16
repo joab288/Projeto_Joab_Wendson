f0a0ac09eee820e13ed09deeb485f61eafe2e7a9
joab288 <joab.santos@estudante.ufla.br>
2019-09-16 20:12:21 -0300

    riação da Classe C

---
+++ C.java
@@ -0,0 +1,22 @@
+package mainz;
+
+/**
+ *
+ * @author joab
+ */
+public class C {
+    private int C1;
+    private float C2;
+
+    public B(int C1, float C2) {
+        this.C1 = C1;
+        this.C2 = C2;
+    }
+    
+    public void MC1(){
+        System.out.println("MC1");
+    }
+    public void MC2(){
+        System.out.println("MC2");
+    }
+}
