47153ba24c7fad23a230e8caa54987ac51cd6f46
joab288 <joab.santos@estudante.ufla.br>
2019-09-16 20:20:41 -0300

    nova classe

---
+++ D.java
@@ -0,0 +1,22 @@
+package mainz;
+
+/**
+ *
+ * @author joab
+ */
+public class D {
+    private int D1;
+    private float D2;
+
+    public D(int D1, float D2) {
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+    
+    public void MD1(){
+        System.out.println("MD1");
+    }
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
