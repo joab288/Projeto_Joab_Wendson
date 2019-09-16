d171d5df4d693a4b1e7020c6937f27ffd49c0c05
joab288 <55414066+joab288@users.noreply.github.com>
2019-09-16 18:31:15 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainz;
+
+/**
+ *
+ * @author joab
+ */
+public class A {
+    private int A1;
+    private float A2;
+
+    public A(int A1, float A2) {
+        this.A1 = A1;
+        this.A2 = A2;
+    }
+
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+    
+    public void MA1(){
+        System.out.println("MA1");
+    }
+    public void MA2(){
+        System.out.println("MA2");
+    }
+    
+}
